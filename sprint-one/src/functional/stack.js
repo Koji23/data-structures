54c35df92afbe0c15d232e9c59a5597fd897a623
Koji23 <jordanchong23@gmail.com>
2016-06-08 15:11:00 -0700

    Implement stack via functional instantiation pattern - pass all tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,28 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var _length = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[_length] = value;
+    _length++;
+    
   };
 
   someInstance.pop = function() {
+    if (_length) {
+      var toRemove = storage[_length - 1];
+      delete storage[_length - 1];
+      _length--;
+      return toRemove;
+    } else {
+      return 0;
+    }
   };
 
   someInstance.size = function() {
+    return _length;
   };
 
   return someInstance;
