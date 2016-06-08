a76451b2dcb7a3c7fa35cab1bd49945477ed0c8a
Koji23 <jordanchong23@gmail.com>
2016-06-08 16:39:11 -0700

    Implement dequeue function and debug - pass all tests

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,12 +1,13 @@
 var Queue = function() {
-
-  return {
+  var storage = {
     enqueue: queueMethods.enqueue,
     dequeue: queueMethods.dequeue,
     size: queueMethods.size,
     length: 0,
     nextKey: 0
   };
+
+  return storage;
 
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
@@ -16,13 +17,20 @@ var queueMethods = {
   //holds queue methods obviously:
 
   enqueue: function(value) {
-    this[this.length] = value;
+    this[this.nextKey] = value;
     this.length++;
     this.nextKey++;
   },
 
   dequeue: function() {
-    
+    if (this.length) {
+      var toDequeue = this[this.nextKey - this.length];
+      delete this[this.nextKey - this.length];
+      this.length--;
+      return toDequeue;
+    } else {
+      return 0;
+    }
   },
   size: function() {
     return this.length;
