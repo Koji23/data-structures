471e6628fdded771634c24d27139c2967fc20edd
Koji23 <jordanchong23@gmail.com>
2016-06-08 15:54:35 -0700

    Declare functional-shared queue interface (private variables and methods)

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
+  var storage = {
+    enqueue: queueMethods.enqueue,
+    dequeue: queueMethods.dequeue,
+    size: queueMethods.size
+  };
+  var _length = 0;
+  var _nextKey = 0;
+
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var queueMethods = {};
+var queueMethods = {
+  //holds queue methods obviously:
+
+  enqueue: function(value) {
+
+  },
+
+  dequeue: function() {
+
+  },
+
+  size: function() {
+
+  }
+};
 
 
