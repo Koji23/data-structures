3c14f27c28dcf1d2799d9708c0cc78cf1edddda8
Koji23 <jordanchong23@gmail.com>
2016-06-08 15:23:31 -0700

    Implement Queue's private variables and Enqueue method

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,10 +3,17 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var _length = 0;
+  var _nextKey = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    //create property on storage using next key
+    //increment length
+    //increment next key
+    storage[_nextKey] = value;
+    _length++;
+    _nextKey++;
   };
 
   someInstance.dequeue = function() {
