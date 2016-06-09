72fe98cddd5cd910367de924ddbfd9323350a78f
Koji23 <jordanchong23@gmail.com>
2016-06-08 21:10:22 -0700

    Implement stack push

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+  instance.lastInd = 0;
+  instance.storage = {};
+
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(val) {
+    this.storage[this.lastInd] = val;
+    this.lastInd++;
+  },
+  pop: function() {
+
+  },
+  size: function() {
+    return this.lastInd;
+  }
+};
 
 
