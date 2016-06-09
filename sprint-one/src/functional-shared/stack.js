8b7d6e531639b7f2116f6e05a369151b9c11c0a8
Koji23 <jordanchong23@gmail.com>
2016-06-08 19:42:06 -0700

    Implement functional-shared stack constructor & push method

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {};
+  instance.storage = {};
+  instance.length = 0;
+
+  _.extend(instance, stackMethods);
+
+  return instance;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(val) {
+  this.storage[this.length] = val;
+  this.length++;
+};
+
+stackMethods.pop = function() {
+};
 
 
