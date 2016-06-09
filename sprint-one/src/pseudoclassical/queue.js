853fe57be86d2e1436ccb954b1190915661f483e
Koji23 <jordanchong23@gmail.com>
2016-06-08 21:22:33 -0700

    Define interface of Queue and implement size

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,23 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.firstInd = 0;
+  this.lastInd = 0;
+
 };
+
+Queue.prototype.enqueue = function(val) {
+
+};
+
+Queue.prototype.dequeue = function() {
+
+};
+
+Queue.prototype.size = function() {
+  return this.lastInd - this.firstInd;
+};
+
 
 
