45261c1e37ecfb285d155e3a1174b30b839fa9aa
Koji23 <jordanchong23@gmail.com>
2016-06-08 21:30:53 -0700

    Define structure for stack and implement push

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,21 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.length = 0;
+};
+
+Stack.prototype.push = function(val) {
+  this.storage[this.length] = val;
+  this.length++;  
+};
+
+Stack.prototype.pop = function() {
+  
+};
+
+Stack.prototype.size = function() {
+  
 };
 
 
