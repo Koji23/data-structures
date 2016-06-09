9ee27da8533d378cc3c9e6e57ec40c61b5b8160e
Koji23 <jordanchong23@gmail.com>
2016-06-08 20:20:21 -0700

    Define structure for queue-prototypal and implement size method

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,26 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = Object.create(queueMethods);
+  instance.length = 0;
+  instance.firstInd = 0;
+  instance.lastInd = 0;
+  instance.storage = {};
+
+  return instance;
 };
 
 var queueMethods = {};
+queueMethods.enqueue = function(val) {
+  //last index moves
+};
+
+queueMethods.dequeue = function() {
+  //first index moves
+};
+
+queueMethods.size = function() {
+  return this.lastInd - this.firstInd;
+};
 
 
