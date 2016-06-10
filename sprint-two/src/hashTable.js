bad90e450afe6b169991eeaf736979532aba17af
Koji23 <jordanchong23@gmail.com>
2016-06-09 20:16:30 -0700

    Reimplement hashtable interface to avoid hash collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,15 +7,22 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  if (this._storage.get(index)) {
+    this._storage.get(index)[k] = v;
+  } else {
+    this._storage.set(index, {});
+    this._storage.get(index)[k] = v;
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index)[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  delete this._storage.get(index)[k];
 };
 
 
