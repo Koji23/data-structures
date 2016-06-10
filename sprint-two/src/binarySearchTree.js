349311a8443a3067cc0cc1026ec5d89623240642
Koji23 <jordanchong23@gmail.com>
2016-06-09 20:35:06 -0700

    Define BST interface and implement insert function

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,24 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+};
+
+BinarySearchTree.prototype.insert = function(value) {
+  var newBST = new BinarySearchTree(value);
+  if (value < this.value) {
+    this.left = newBST;
+  } else {
+    this.right = newBST;
+  }
+};
+
+BinarySearchTree.prototype.contains = function(target) {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+
 };
 
 
