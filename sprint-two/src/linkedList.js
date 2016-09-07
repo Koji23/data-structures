7f7ff9e83c6520469cd876292d22a00039aed6a7
Jordan Chong <jordanchong23@gmail.com>
2016-09-07 13:23:55 -0700

    complete linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,17 +4,39 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
-    list.tail 
-  };
+    var newNode = Node(value);
+    if (list.tail) {
+      list.tail.next = newNode;
+    } else {
+      list.head = newNode; // if list.tail doesn't exist then list.head doesn't either
+    }
+    list.tail = newNode;
+  }; 
+  // O(1)
 
   list.removeHead = function() {
-  };
+    if (list.head) {
+      var temp = list.head;
+      list.head = list.head.next;
+      return temp.value;
+    }
+  }; 
+  // O(1)
 
   list.contains = function(target) {
+    var current = list.head;
+    while (current.next) {
+      if (current.value === target) {
+        return true;
+      }
+      current = current.next;
+    }
+    return current.value === target;
   };
 
   return list;
-};
+}; 
+// O(n) 
 
 var Node = function(value) {
   var node = {};
