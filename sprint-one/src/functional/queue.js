8cdfaf8f0a28d859f02e94ca001744877f837bf8
Jordan Chong <jordanchong23@gmail.com>
2016-09-07 11:04:25 -0700

    complete functional stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,26 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
+  someInstance.head = 0;
+  someInstance.tail = 0;
 
   someInstance.enqueue = function(value) {
+    storage[tail] = value;
+    tail++;
+    return storage;
   };
 
   someInstance.dequeue = function() {
+    if (head !== tail) {
+      var temp = storage[head];
+      delete storage[head];
+      head++;
+      return temp;
+    }
   };
 
   someInstance.size = function() {
+    return tail - head >= 0 ? (tail - head) : 0;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,26 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  someInstance.tail = 0;
+
   // Implement the methods below
   someInstance.push = function(value) {
+    tail++;
+    storage[tail] = value;
+    return storage;
   };
 
   someInstance.pop = function() {
+    if (tail > 0) {
+      var temp = storage[tail];
+      delete storage[tail];
+      tail--;
+      return temp;
+    }
   };
 
   someInstance.size = function() {
+    return tail >= 0 ? tail : 0;
   };
 
   return someInstance;
