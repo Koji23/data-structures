a5473857b53da6e9a45f7fde40a8680488fc3edf
Jordan Chong <jordanchong23@gmail.com>
2016-09-07 15:01:04 -0700

    complete regular hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,43 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index) || []; // handle tuplecontainer doesn't exist
+  var updated = false;
+  console.log(bucket, k, v);
+  for (var i = 0; i < bucket.length; i++) {
+    // debugger;
+    if (bucket[i][0] === k) {
+      bucket[i][1] = v; // handle tuple will update
+      console.log(bucket[i]);
+      updated = true;
+    }
+    // debugger;
+  }
+  if (!updated) {
+    bucket.push([k, v]); // handle new tuple
+  }
+  this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  for (var i = 0; i < bucket.length; i++ ) {
+    if (bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  if (bucket) {
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        bucket.splice(i, 1);
+      }
+    }
+  }
 };
-
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
-
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -9,12 +9,12 @@ var setPrototype = {};
 setPrototype.add = function(item) {
   this._storage[item] = true;
 };
-//
+// O(1)
 setPrototype.contains = function(item) {
   return !!this._storage[item];
 };
-//
+// O(1)
 setPrototype.remove = function(item) {
   delete this._storage[item];
 };
-//
+// O(1)
