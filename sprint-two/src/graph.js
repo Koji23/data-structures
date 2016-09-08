128a2971f3570470fd1ff30ee99a37841a417cc0
Jordan Chong <jordanchong23@gmail.com>
2016-09-07 18:31:36 -0700

    complete resizing hashtable and graph (numerical keys

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,40 +1,56 @@
 
 // undirected graph
 var Graph = function(val) {
-  this.value = val;
-  this.edges = [];
+  this.storage = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  node = new Graph(node);
-  this.edges = node;
-  node.edges = this;
+  this.storage[node] = {};
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  
+  for (var key in this.storage) {
+    if (+key === node) {  // only works for numerical keys
+      return true;
+    }
+  }
+  return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  delete this.storage[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  for (var key in this.storage[fromNode]) {
+    if (+key === toNode) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.storage[fromNode][toNode] = this.storage[toNode];
+  this.storage[toNode][fromNode] = this.storage[fromNode];
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  delete this.storage[fromNode][toNode];
+  delete this.storage[toNode][fromNode];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var key in this.storage) {
+    cb(+key);
+  }
 };
 
 /*
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,31 +3,48 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._size = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
+  if (this._size >= this._limit * 0.75) {
+    // handle a resize if tuple count is over 75% of storage length
+    var oldStorage = this._storage;
+    var context = this; // 'this' cannot be accessed inside of the limitedArray's each meethod
+    this._limit *= 2;
+    this._storage = LimitedArray(this._limit);
+    this._size = 0;
+    // now rehash all existing tuples
+    oldStorage.each(function(bucket) {
+      bucket = bucket || [];
+      for (var i = 0; i < bucket.length; i++) {
+        context.insert(bucket[i][0], bucket[i][1]);
+      }
+    });
+  }
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index) || []; // handle tuplecontainer doesn't exist
-  var updated = false;
-  console.log(bucket, k, v);
+  var updated = null;
   for (var i = 0; i < bucket.length; i++) {
     // debugger;
     if (bucket[i][0] === k) {
+      updated = bucket[i][1];
       bucket[i][1] = v; // handle tuple will update
-      console.log(bucket[i]);
-      updated = true;
     }
     // debugger;
   }
-  if (!updated) {
+  if (updated === null) {
     bucket.push([k, v]); // handle new tuple
+    this._size++;
+  } else {
+    return updated;
   }
   this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage.get(index);
+  var bucket = this._storage.get(index) || [];
   for (var i = 0; i < bucket.length; i++ ) {
     if (bucket[i][0] === k) {
       return bucket[i][1];
@@ -36,14 +53,29 @@ HashTable.prototype.retrieve = function(k) {
 };
 
 HashTable.prototype.remove = function(k) {
+  // now rehash all existing tuples
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var bucket = this._storage.get(index);
-  if (bucket) {
-    for (var i = 0; i < bucket.length; i++) {
-      if (bucket[i][0] === k) {
-        bucket.splice(i, 1);
-      }
+  var bucket = this._storage.get(index) || [];
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][0] === k) {
+      bucket.splice(i, 1);
+      this._size--;
     }
+  }
+  if (this._size < this._limit * 0.25) {  // note: '<' not '<=' in order to pass tests
+    // handle resize for tuple count under 25% of storage length (to save space)
+    var context = this;
+    var oldStorage = this._storage;
+    this._limit /= 2;
+    this._storage = LimitedArray(this._limit);
+    this._size = 0;
+    oldStorage.each(function(bucket) {
+      bucket = bucket || [];
+      for (var i = 0; i < bucket.length; i++) {
+        context.insert(bucket[i][0], bucket[i][1]);
+      }
+    });
+
   }
 };
 
